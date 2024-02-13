6d4010803e1b1ebba448304c02d5cf8bafcfdfae
Ezan-ul-haque <159648579+Ezan-ul-haque@users.noreply.github.com>
2024-02-14 01:53:00 +0530

    Add files via upload

---
+++ Dc_ASSIGNMENT_INPUTS.m
@@ -0,0 +1,6 @@
+
+J=0.01;%moment of inertia of rotor (kg/m^2)
+B=0.1;%damping
+K=0.01;%back emf cosntant (v/rad/s)
+R=1.0;% Resistance of the armature circuit in (ohms)
+L=0.5;%Inductacne of the armature current in (henries)
